db969bdcee722d747e2616f85cbf03e69bcdc003
Antonio Alan <antonio45alan@outlook.com>
2023-10-07 19:27:12 -0400

    atualização3

---
+++ cursos/Java script/exercicios JS/ex005/index.js
@@ -0,0 +1,14 @@
+var velo = 70
+console.log(`A velocidade do seu carro é ${velo} km/h`)
+if(velo > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil') {
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log('Você é Estrangeiro!')
+}
